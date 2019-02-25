0543f2598e0346cad69c03506739a57efec1a8ec
mason <9265959@qq.com>
2019-02-25 15:27:33 +0800

    头部和底部数据渲染

--- src/components/layouts/TheFooter.vue
+++ src/components/layouts/TheFooter.vue
@@ -9,50 +9,133 @@
           </dd>
         </dl>
         <dl class="col-xs-2">
-          <dt>开发宝典</dt>
-          <dd>PHP手册</dd>
-          <dd>Laravel手册</dd>
-          <dd>Mysql手册</dd>
+          <dt>{{dictionary.title}}</dt>
+          <dd v-for="item in dictionary.list" :key="item.num">
+              <a href="{item.link}">{{item.name}}</a>
+          </dd>
         </dl>
         <dl class="col-xs-2">
-          <dt>学习路线</dt>
-          <dd>Linux学习指南</dd>
-          <dd>Laravel学习指南</dd>
-          <dd>Python学习指南</dd>
-          <dd>Mysql学习指南</dd>
+          <dt>{{learningWay.title}}</dt>
+          <dd v-for="item in learningWay.list" :key="item.num">
+              <a href="{item.link}">{{item.name}}</a>
+          </dd>
         </dl>
         <dl class="col-xs-2">
-          <dt>面试王中王</dt>
-          <dd>PHP面试模板</dd>
-          <dd>简历模板</dd>
-          <dd>Mysql面试宝典</dd>
-          <dd>Linux面试宝典</dd>
+          <dt>{{interView.title}}</dt>
+          <dd v-for="item in interView.list" :key="item.num">
+              <a href="{item.link}">{{item.name}}</a>
+          </dd>
         </dl>
         <dl class="col-xs-2">
-          <dt>资源推荐</dt>
-          <dd>暂无内容~</dd>
+          <dt>{{source.title}}</dt>
+          <dd v-for="item in source.list" :key="item.num">
+              <a href="{item.link}">{{item.name}}</a>
+          </dd>
         </dl>
         <dl class="col-xs-2">
-          <img src="../../assets/QRcode.jpg" alt="Ans-小锋" title="Ans-小锋" class="img-responsive img-rounded">
+          <img
+            src="../../assets/QRcode.jpg"
+            alt="Ans-小锋"
+            title="Ans-小锋"
+            class="img-responsive img-rounded"
+          >
           <div class="contact text-center">
-              <p>扫一扫 contact me</p>
+            <p>扫一扫 contact me</p>
           </div>
         </dl>
       </div>
     </div>
-    <div class="copyright">
-      <div class="container">
+    <div class="copyright" v-html="copyright"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      copyright: `
+       <div class="container">
         <strong>Copyright</strong> © 2018 友叹-USIGH - All rights reserved - 冀ICP备18012344号
         <div class="pull-right">
           <strong>www.usigh.com</strong>
         </div>
       </div>
-    </div>
-  </div>
-</template>
-
-<script>
-export default {};
+      `,
+      dictionary: {
+        title: "开发宝典",
+        list: [
+          {
+            name: "PHP手册",
+            link: "#"
+          },
+          {
+            name: "Laravel手册",
+            link: "#"
+          },
+          {
+            name: "Mysql手册",
+            link: "#"
+          }
+        ]
+      },
+      learningWay: {
+        title: "学习路线",
+        list: [
+          {
+            name: "Laravel学习指南",
+            link: "#"
+          },
+          {
+            name: "PHP学习指南",
+            link: "#"
+          },
+          {
+            name: "Mysql学习指南",
+            link: "#"
+          },
+          {
+            name: "ThinkPHP学习指南",
+            link: "#"
+          },
+          {
+            name: "Linux学习指南",
+            link: "#"
+          }
+        ]
+      },
+      interView: {
+        title: "面试王中王",
+        list: [
+          {
+            name: "PHP面试宝典",
+            link: "#"
+          },
+          {
+            name: "简历模板",
+            link: "#"
+          },
+          {
+            name: "Mysql面试宝典",
+            link: "#"
+          },
+          {
+            name: "Linux面试宝典",
+            link: "#"
+          }
+        ]
+      },
+      source: {
+        title: "资源推荐",
+        list: [
+          {
+            name: "暂无资源~",
+            link: "#"
+          }
+        ]
+      }
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
@@ -73,10 +156,17 @@ export default {};
     dd {
       margin: 5px 0;
       color: #676a6c;
+      a {
+        text-decoration: none;
+        color: #676a6c;
+        &:hover {
+          color: #ccc;
+        }
+      }
     }
   }
   .copyright {
-    padding: 6px 0;
+    padding: 3px 0;
     line-height: 40px;
     border-top: 1px solid #e7eaec;
   }
